a2771a0758bd3bf70c9aa8e47d98eb7d377242f7
Pashaie <pashaie@gmail.com>
2025-03-03 19:00:57 +0330

    multi thread

--- Session09/Form1.Designer.cs
+++ Session09/Form1.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            buttonSendEmail = new Button();
+            buttonTestMsg = new Button();
+            buttonSendEmailMultiThread = new Button();
+            progressBarEmails = new ProgressBar();
+            SuspendLayout();
+            // 
+            // buttonSendEmail
+            // 
+            buttonSendEmail.Location = new Point(12, 12);
+            buttonSendEmail.Name = "buttonSendEmail";
+            buttonSendEmail.Size = new Size(161, 50);
+            buttonSendEmail.TabIndex = 0;
+            buttonSendEmail.Text = "Send Email";
+            buttonSendEmail.UseVisualStyleBackColor = true;
+            buttonSendEmail.Click += buttonSendEmail_Click;
+            // 
+            // buttonTestMsg
+            // 
+            buttonTestMsg.Location = new Point(594, 12);
+            buttonTestMsg.Name = "buttonTestMsg";
+            buttonTestMsg.Size = new Size(194, 50);
+            buttonTestMsg.TabIndex = 1;
+            buttonTestMsg.Text = "Test MSG";
+            buttonTestMsg.UseVisualStyleBackColor = true;
+            buttonTestMsg.Click += buttonTestMsg_Click;
+            // 
+            // buttonSendEmailMultiThread
+            // 
+            buttonSendEmailMultiThread.Location = new Point(12, 68);
+            buttonSendEmailMultiThread.Name = "buttonSendEmailMultiThread";
+            buttonSendEmailMultiThread.Size = new Size(161, 50);
+            buttonSendEmailMultiThread.TabIndex = 2;
+            buttonSendEmailMultiThread.Text = "Send Email - Multi Thread";
+            buttonSendEmailMultiThread.UseVisualStyleBackColor = true;
+            buttonSendEmailMultiThread.Click += buttonSendEmailMultiThread_Click;
+            // 
+            // progressBarEmails
+            // 
+            progressBarEmails.Location = new Point(12, 124);
+            progressBarEmails.Name = "progressBarEmails";
+            progressBarEmails.Size = new Size(161, 23);
+            progressBarEmails.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(progressBarEmails);
+            Controls.Add(buttonSendEmailMultiThread);
+            Controls.Add(buttonTestMsg);
+            Controls.Add(buttonSendEmail);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button buttonSendEmail;
+        private Button buttonTestMsg;
+        private Button buttonSendEmailMultiThread;
+        private ProgressBar progressBarEmails;
     }
 }
--- Session09/Form1.cs
+++ Session09/Form1.cs
@@ -1,10 +1,42 @@
-namespace Session09
+﻿namespace Session09
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonSendEmail_Click(object sender, EventArgs e)
+        {
+            //Multi Task
+
+            //Thread / نخ            
+            //Concurrent
+
+            sendEmail();
+        }
+
+        private void sendEmail()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                //Thread.Sleep(1000); //1000ms = 1s;
+                Thread.Sleep(100); //100ms = 0.1s;
+                //progressBarEmails.Value = i + 1;
+                progressBarEmails.Invoke(() => progressBarEmails.Value = i + 1);
+            }
+        }
+
+        private void buttonTestMsg_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
+
+        private void buttonSendEmailMultiThread_Click(object sender, EventArgs e)
+        {
+            var t = new Thread(() => sendEmail());
+            t.Start();
         }
     }
 }
