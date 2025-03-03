f2b106acca93294edd2c19be3239d661a185f63c
Pashaie <pashaie@gmail.com>
2025-03-03 21:16:57 +0330

    soap server

---
+++ SoapServer/CalculatorWebService.asmx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace SoapServer
+{
+    /// <summary>
+    /// Summary description for CalculatorWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalculatorWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        [WebMethod]
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        [WebMethod]
+        public int Div(int a, int b)
+        {
+            return a / b;
+        }
+
+
+        [WebMethod]
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
